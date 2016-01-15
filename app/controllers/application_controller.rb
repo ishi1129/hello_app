8d4d7efbc1d735c3f6809f891bc363e2d671084c
T.I <taka1129.zzz@hotmail.co.jp>
2016-01-15 17:06:24 +0900

    test2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def Hello
-    render text: "hello world"
+    render text: "¡Hola, mundo!"
+  end
+  
+  def Goodbye
+    render text: "goodbye, world!"
   end
 end
